319a162fd021cc5c3c8164aedbfbf35580c4e5de
垣井駿努 <syunto07ka@yahoo.co.jp>
2019-05-31 00:30:09 +0900

    feat: lambda用に変更

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
-	"./serverRun"
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
-	serverRun.Start()
+	lambda.Start(pushMessage)
+}
+
+func pushMessage(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	name := "カッキー"   // request.PathParameters["name"]
+	message := "わーい" // request.QueryStringParameters["message"]
+
+	chat := Chat{
+		Username: name,
+		Text:     message,
+	}
+
+	jsonMessage, err := json.Marshal(chat)
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(jsonMessage),
+		StatusCode: 200,
+	}, nil
+}
+
+// Chat is a message.
+type Chat struct {
+	Username string `json:"username"`
+	Text     string `json:"text"`
 }
